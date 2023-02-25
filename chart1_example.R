676242c8663a565a2bb41e020627c990ea2e6c58
jechow <jechow@uw.edu>
2023-02-24 23:34:02 -0800

    final changes

--- chart1_example.R
+++ chart1_example.R
@@ -3,7 +3,7 @@ library(ggplot2)
 
 checkouts_df <- read.csv("C:/Users/hello/Downloads/2017-2023-10-Checkouts-SPL-Data.csv", stringsAsFactors = F)
 
-x_and_y_values <- checkouts_df %>% group_by(MaterialType, CheckoutYear) %>% filter(MaterialType %in% c("BOOK", "EBOOK", "AUDIOBOOK", "VIDEODISC", "SOUNDDISC")) %>% summarize(Checkouts = sum(Checkouts))  
+x_and_y_values <- checkouts_df %>% group_by(MaterialType, CheckoutYear) %>% filter(MaterialType %in% c("BOOK", "EBOOK", "AUDIOBOOK", "VIDEODISC", "SOUNDDISC") & CheckoutYear < 2023) %>% summarize(Checkouts = sum(Checkouts))  
 
 ggplot(data = x_and_y_values) +
   xlab("Year") + ggtitle("Checkouts by Type") +
